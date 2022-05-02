347dff250d9c3117b6ebbd0aca76c09bd683870c
tetsup <58599705+tetsup@users.noreply.github.com>
2022-05-02 10:26:26 +0900

    コントローラのi18nを相対表記にそろえる

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
     @project = @mission.projects.build(project_params)
     @project.project_user_relationships.build(user: current_user, role: :owner)
     if @project.save
-      redirect_to edit_mission_path(params[:mission_id]), notice: t('.project.create.succeeded')
+      redirect_to edit_mission_path(params[:mission_id]), notice: t('.succeeded')
     else
       flash[:alert] = t('.failed')
       render 'new'
@@ -31,9 +31,9 @@ class ProjectsController < ApplicationController
 
   def destroy
     if @project.destroy
-      redirect_back fallback_location: missions_path, notice: t('.mission.destroy.succeeded')
+      redirect_back fallback_location: missions_path, notice: t('.succeeded')
     else
-      flash[:warning] = t('.mission.destroy.failed')
+      flash[:warning] = t('.failed')
     end
   end
 
