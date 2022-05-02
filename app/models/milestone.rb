4775274377d01d126b6bb7954e8904eafaeefab7
tetsup <58599705+tetsup@users.noreply.github.com>
2022-05-02 14:41:45 +0900

    マイルストーンにステータス別タスク件数追加

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,7 +1,22 @@
 class Milestone < ApplicationRecord
   has_many :requirements, dependent: :destroy
+  has_many :tasks, through: :requirements
   belongs_to :project
 
   enum status: %i[notyet doing done]
   validates :name, presence: true, uniqueness: { scope: :project_id }, length: { maximum: 50 }
+
+  def count_tasks
+    tasks.count
+  end
+
+  def count_tasks_by_status(status)
+    tasks.filter_by_status(status).count
+  end
+
+  def count_all_tasks_by_status
+    Task.statuses.keys.map do |s|
+      count_tasks_by_status(s)
+    end
+  end
 end
