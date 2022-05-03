dd3f5159616e3222b9b6055c45fe46bb85800beb
tetsup <58599705+tetsup@users.noreply.github.com>
2022-05-04 06:51:42 +0900

    rspec設定変更

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,13 @@ module App
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+    end
   end
 end
