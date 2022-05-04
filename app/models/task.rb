3300265d5fc9bf42516b0b2d01ce95efd5e36555
tetsup <58599705+tetsup@users.noreply.github.com>
2022-05-05 01:03:53 +0900

    不要なスペース削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,12 +5,12 @@ class Task < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :requirement_id }, length: { maximum: 20 }
   validates :description, length: { maximum: 200 }
 
-  scope :filter_by_status, -> (status) { where(status:) }
+  scope :filter_by_status, ->(status) { where(status:) }
 
   def planned
     "#{I18n.l(planned_start_date)} - #{I18n.l(planned_end_date, format: :short)}"
   end
-  
+
   def actual
     "#{I18n.l(started_date)} - #{I18n.l(ended_date, format: :short)}"
   end
