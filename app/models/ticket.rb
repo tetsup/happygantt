2065673cd6c6a7106204085155693d46fe5657bc
tetsup <58599705+tetsup@users.noreply.github.com>
2022-05-05 15:56:46 +0900

    チケットのステータス空制約追加

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,6 +4,7 @@ class Ticket < ApplicationRecord
   enum status: %i[notyet doing done]
   validates :name, presence: true, uniqueness: { scope: :requirement_id }, length: { maximum: 20 }
   validates :description, length: { maximum: 200 }
+  validates :status, presence: true
 
   scope :filter_by_status, ->(status) { where(status:) }
 
