0a49afeee3781211eb9e69bd5a257b63982bff65
tetsup <58599705+tetsup@users.noreply.github.com>
2022-05-05 23:50:41 +0900

    プロジェクト作成と変更のシステムテスト

---
+++ spec/system/projects_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Projects', type: :system do
+  let(:user) { create(:user) }
+  let(:mission_user_relationship) { create(:mission_user_relationship, user:) }
+  let(:mission) { mission_user_relationship.mission }
+  let(:project) { create(:project, mission:) }
+
+  before do
+    sign_in_as user
+  end
+
+  it 'creates a new project as a user' do
+    expect {
+      visit new_mission_project_path(mission_id: mission.id)
+      fill_in 'project[name]', with: 'やさしさを拡散する'
+      select Project.statuses_i18n[:notyet], from: 'project[status]'
+      click_button 'commit'
+      expect(page).to have_content I18n.t('projects.create.succeeded')
+    }.to change(user.projects, :count).by(1)
+  end
+
+  it 'updates status of a project as a user' do
+    visit edit_project_path(id: project.id)
+    select Project.statuses_i18n[:doing], from: 'project[status]'
+    click_button 'commit'
+    expect(page).to have_content I18n.t('projects.update.succeeded')
+    expect(project.reload.status).to eq 'doing'
+  end
+end
